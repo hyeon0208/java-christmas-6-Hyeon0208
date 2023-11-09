3219cacbf397723644076da14040cb0897cd377d
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-09 22:06:50 +0900

    feat(OrderInputValidator) : 구분자로 쉼표를 사용했는지 검증 기능 추가

--- src/main/java/christmas/validation/OrderInputValidator.java
+++ src/main/java/christmas/validation/OrderInputValidator.java
@@ -2,16 +2,24 @@ package christmas.validation;
 
 public class OrderInputValidator {
     private static final String ORDER_INPUT_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String COMMA = ",";
 
     private OrderInputValidator() {
     }
 
     public static void validate(String input) {
         validateEmpty(input);
+        validateDelimiter(input);
     }
 
     private static void validateEmpty(String input) {
         if (input.isEmpty()) {
+            throw new IllegalArgumentException(ORDER_INPUT_ERROR);
+        }
+    }
+
+    private static void validateDelimiter(String input) {
+        if (!input.contains(COMMA)) {
             throw new IllegalArgumentException(ORDER_INPUT_ERROR);
         }
     }
