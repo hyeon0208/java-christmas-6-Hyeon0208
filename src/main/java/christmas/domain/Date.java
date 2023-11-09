20dafaba0a87e8f346a5176fe6a5868407c81080
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-09 21:27:18 +0900

    feat(Date) : 방문 날짜 범위 검증 기능 추가

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -3,6 +3,8 @@ package christmas.domain;
 import christmas.util.Convertor;
 
 public class Date {
+    private static final int START_DATE = 1;
+    private static final int END_DATE = 31;
     private final int visitDate;
 
     private Date(int visitDate) {
@@ -11,7 +13,14 @@ public class Date {
 
     public static Date from(String input) {
         int visitDate = Convertor.convertStringToInt(input);
+        validateRange(visitDate);
         return new Date(visitDate);
+    }
+
+    private static void validateRange(int date) {
+        if (date < START_DATE || date > END_DATE) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
     public int getVisitDate() {
