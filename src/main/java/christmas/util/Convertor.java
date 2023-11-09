c96677712389968bc60f010d090bb39706f20ca1
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-09 21:21:59 +0900

    feat(Convertor) : 변한 기능을 담당하는 유틸 클래스 추가

---
+++ src/main/java/christmas/util/Convertor.java
@@ -0,0 +1,11 @@
+package christmas.util;
+
+public class Convertor {
+
+    private Convertor() {
+    }
+
+    public static int convertStringToInt(String value) {
+        return Integer.parseInt(value);
+    }
+}
