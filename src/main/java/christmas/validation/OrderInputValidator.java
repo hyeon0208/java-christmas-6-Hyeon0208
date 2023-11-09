b2a7a79375895ac12272f9ae287dd854f941874b
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-10 00:12:36 +0900

    feat(OrderInputValidator) : 주문 메뉴 입력 형식 검증 기능 추가

--- src/main/java/christmas/validation/OrderInputValidator.java
+++ src/main/java/christmas/validation/OrderInputValidator.java
@@ -1,10 +1,15 @@
 package christmas.validation;
 
 import christmas.constant.ErrorMessage;
+import christmas.util.Convertor;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class OrderInputValidator {
+    private static final Pattern ONLY_NUMBER = Pattern.compile("\\d+");
+    private static final int MENU_INFO_SIZE = 2;
     private static final String COMMA = ",";
+    private static final String HYPHEN = "-";
 
     private OrderInputValidator() {
     }
@@ -12,6 +17,7 @@ public class OrderInputValidator {
     public static void validate(String input) {
         validateEmpty(input);
         validateComma(input);
+        validateOrderMenus(input);
     }
 
     private static void validateEmpty(String input) {
@@ -24,5 +30,36 @@ public class OrderInputValidator {
         if (!input.contains(COMMA)) {
             throw new IllegalArgumentException(ErrorMessage.ORDER_ERROR);
         }
+    }
+
+    private static void validateOrderMenus(String input) {
+        String[] orderMenus = Convertor.splitByComma(input);
+        validateOrderMenuFormat(orderMenus);
+    }
+
+    private static void validateOrderMenuFormat(String[] orderMenus) {
+        validateHyphen(orderMenus);
+        for (String orderMenu : orderMenus) {
+            String[] menuInfo = Convertor.splitByHyphen(orderMenu);
+            String price = menuInfo[1];
+            if (menuInfo.length != MENU_INFO_SIZE || !isNumber(price)) {
+                throw new IllegalArgumentException(ErrorMessage.ORDER_ERROR);
+            }
+        }
+    }
+
+    private static void validateHyphen(String[] orderMenus) {
+        if (isNotContainsHyphen(orderMenus)) {
+            throw new IllegalArgumentException(ErrorMessage.ORDER_ERROR);
+        }
+    }
+
+    private static boolean isNotContainsHyphen(String[] orderMenus) {
+        return Arrays.stream(orderMenus)
+                .anyMatch(orderMenu -> !orderMenu.contains(HYPHEN));
+    }
+
+    private static boolean isNumber(String price) {
+        return ONLY_NUMBER.matcher(price).matches();
     }
 }
