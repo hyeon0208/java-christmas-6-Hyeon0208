b7606bbc0f87c4a29b33b0ad57d1417184df9472
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-10 18:49:21 +0900

    test(DateTest) : 평일, 주말 그리고 특별한 날을 확인하는 테스트 추가

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -18,6 +18,34 @@ class DateTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("12월의 금요일,토요일은 주말이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "8", "9", "15", "16", "22", "23", "29", "30"})
+    void isWeekdayTest(String date) {
+        // given
+        Date visitDate = Date.from(date);
+
+        // when
+        boolean weekday = visitDate.isWeekday();
+
+        // then
+        assertThat(weekday).isFalse();
+    }
+
+    @DisplayName("3, 10, 17, 24, 25, 31일는 특별한 날이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "10", "17", "24", "25", "31"})
+    void isSpecialDayTest(String date) {
+        // given
+        Date visitDate = Date.from(date);
+
+        // when
+        boolean specialDay = visitDate.isSpecialDay();
+
+        // then
+        assertThat(specialDay).isTrue();
+    }
+
     @DisplayName("방문 날짜를 가져온다.")
     @Test
     void getVisitDateTest() {
