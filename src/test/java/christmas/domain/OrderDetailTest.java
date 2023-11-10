a8449fb85f9843da64b4b3af6f60fcac1a7f150d
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-10 17:18:31 +0900

    test(OrderDetail) : 주문 상세 도메인 테스트 추가

---
+++ src/test/java/christmas/domain/OrderDetailTest.java
@@ -0,0 +1,47 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderDetailTest {
+
+    @DisplayName("메뉴판에 존재하지 않는 메뉴를 주문하면 예외가 발생한다.")
+    @Test
+    void validateExistMenuTest() {
+        // given
+        String orderMenu = "없는메뉴-1";
+
+        // then
+        assertThatThrownBy(() -> OrderDetail.of(orderMenu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴의 주문 개수가 1개보다 작으면 예외가 발생한다.")
+    @Test
+    void validateOrderQuantityTest() {
+        // given
+        String orderMenu = "티본스테이크-0";
+
+        // then
+        assertThatThrownBy(() -> OrderDetail.of(orderMenu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문한 메뉴 가격과 개수를 곱한 주문 금액을 구한다.")
+    @Test
+    void getOrderPriceTest() {
+        // given
+        String orderMenu = "크리스마스파스타-2";
+
+        // when
+        OrderDetail orderDetail = OrderDetail.of(orderMenu);
+        int orderPrice = orderDetail.getOrderPrice();
+        int expectedOrderPrice = 50000;
+
+        // then
+        assertThat(orderPrice).isEqualTo(expectedOrderPrice);
+    }
+}
