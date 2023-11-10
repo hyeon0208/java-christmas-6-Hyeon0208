8407f74a6b1713e68310b0171ec4419aa66dfbd0
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-10 23:41:19 +0900

    refactor(Menu) : 메뉴 추가 메서드에 2차원 배열을 받아 간결하게 나타내도록 수정

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,14 +1,16 @@
 package christmas.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public enum Menu {
-    APPETIZER(addMenus("양송이수프", 6000, "타파스", 5500, "시저샐러드", 8000)),
-    MAIN(addMenus("티본스테이크", 55000, "바비큐립", 54000, "해산물파스타", 35000, "크리스마스파스타", 25000)),
-    DESSERT(addMenus("초코케이크", 15000, "아이스크림", 5000)),
-    DRINK(addMenus("제로콜라", 3000, "레드와인", 60000, "샴페인", 25000));
+    APPETIZER(addMenus(new Object[][] {{"양송이수프", 6000}, {"타파스", 5500}, {"시저샐러드", 8000}})),
+    MAIN(addMenus(new Object[][] {{"티본스테이크", 55000}, {"바비큐립", 54000}, {"해산물파스타", 35000}, {"크리스마스파스타", 25000}})),
+    DESSERT(addMenus(new Object[][] {{"초코케이크", 15000}, {"아이스크림", 5000}})),
+    DRINK(addMenus(new Object[][] {{"제로콜라", 3000}, {"레드와인", 60000}, {"샴페인", 25000}}));
+
 
     private final List<MenuInfo> menuInfos;
 
@@ -26,36 +28,32 @@ public enum Menu {
         return EnumSet.allOf(Menu.class).stream()
                 .flatMap(menu -> menu.menuInfos.stream())
                 .filter(menuInfo -> menuInfo.nameEquals(name))
-                .map(MenuInfo::price)
+                .map(MenuInfo::getPrice)
                 .findFirst()
                 .orElse(0);
     }
 
     public static boolean isDrinkMenu(String name) {
         return DRINK.menuInfos.stream()
-                .map(MenuInfo::name)
+                .map(MenuInfo::getName)
                 .anyMatch(drinkName -> drinkName.equals(name));
     }
 
     public static boolean isDessert(String name) {
         return DESSERT.menuInfos.stream()
-                .map(MenuInfo::name)
+                .map(MenuInfo::getName)
                 .anyMatch(drinkName -> drinkName.equals(name));
     }
 
     public static boolean isMain(String name) {
         return MAIN.menuInfos.stream()
-                .map(MenuInfo::name)
+                .map(MenuInfo::getName)
                 .anyMatch(drinkName -> drinkName.equals(name));
     }
 
-    private static List<MenuInfo> addMenus(Object... menus) {
-        List<MenuInfo> menuItems = new ArrayList<>();
-        for (int i = 0; i < menus.length; i += 2) {
-            String name = (String) menus[i];
-            int price = (int) menus[i + 1];
-            menuItems.add(new MenuInfo(name, price));
-        }
-        return menuItems;
+    private static List<MenuInfo> addMenus(Object[][] menus) {
+        return Arrays.stream(menus)
+                .map(menu -> MenuInfo.from(menu))
+                .collect(Collectors.toList());
     }
 }
