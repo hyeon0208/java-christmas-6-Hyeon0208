f5bb93eb64b477bc71f8ed53804c8460315f6ff0
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-12 21:37:43 +0900

    feat(EventApplicator) : 적용가능한 이벤트를 관리하는 이벤트적용기 객체 추가

---
+++ src/main/java/christmas/domain/EventApplicator.java
@@ -0,0 +1,32 @@
+package christmas.domain;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EventApplicator {
+    private static final int MIN_EVENT_APPLICATION_CONDITIONS = 10000;
+
+    private EventApplicator() {
+    }
+
+    public static Benefit applyEvents(User user) {
+        List<EventInfo> discounts = EnumSet.allOf(Event.class).stream()
+                .filter(event -> isMeetConditionsForEvent(user) && event.isApplicable(user))
+                .map(event -> createEventInfo(user, event))
+                .collect(Collectors.toList());
+        return new Benefit(discounts);
+    }
+
+    private static boolean isMeetConditionsForEvent(User user) {
+        if (user.isOrderedGe(MIN_EVENT_APPLICATION_CONDITIONS)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static EventInfo createEventInfo(User user, Event event) {
+        int discount = event.calculateDiscount(user);
+        return new EventInfo(event.getName(), discount);
+    }
+}
