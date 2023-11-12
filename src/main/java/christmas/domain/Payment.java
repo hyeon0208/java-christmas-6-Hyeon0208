e0bff2f53d083ad48d63945111d10b9c9c3f406c
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-12 21:40:06 +0900

    feat(Payment) : 결제 정보를 관리하는 도메인 객체 추가

---
+++ src/main/java/christmas/domain/Payment.java
@@ -0,0 +1,17 @@
+package christmas.domain;
+
+public class Payment {
+    private final User user;
+    private final Benefit benefit;
+
+    public Payment(User user, Benefit benefit) {
+        this.user = user;
+        this.benefit = benefit;
+    }
+
+    public int getActualPaymentPrice() {
+        int totalOrderPrice = user.getTotalOrderPrice();
+        int totalBenefit = benefit.getTotalBenefitPrice();
+        return totalOrderPrice - totalBenefit;
+    }
+}
