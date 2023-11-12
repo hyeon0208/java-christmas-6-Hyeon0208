cd49c95e087edfedc317e4a0e41142b68885e5ed
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-12 16:27:24 +0900

    refactor(Gift) : 조건에 맞는 증정품을 찾아 Optional로 반환하도록 수정 + 메서드명 수정

--- src/main/java/christmas/domain/Gift.java
+++ src/main/java/christmas/domain/Gift.java
@@ -1,18 +1,37 @@
 package christmas.domain;
 
 import java.util.EnumSet;
+import java.util.Optional;
 
 public enum Gift {
-    CHAMPAGNE(120000);
+    CHAMPAGNE("샴페인", 1, 120000);
 
+    private final String product;
+    private final int quantity;
     private final int minOrderPrice;
 
-    Gift(int minOrderPrice) {
+    Gift(String product, int quantity, int minOrderPrice) {
+        this.product = product;
+        this.quantity = quantity;
         this.minOrderPrice = minOrderPrice;
     }
 
-    public static boolean matches(int totalOrderPrice) {
+    public static boolean canReceive(int totalOrderPrice) {
         return EnumSet.allOf(Gift.class).stream()
                 .anyMatch(gift -> totalOrderPrice >= gift.minOrderPrice);
+    }
+
+    public static Optional<Gift> findByTotalOrderPrice(int totalOrderPrice) {
+        return EnumSet.allOf(Gift.class).stream()
+                .filter(gift -> totalOrderPrice >= gift.minOrderPrice)
+                .findFirst();
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
