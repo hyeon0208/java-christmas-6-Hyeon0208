db5141a585b9114840bd3fd0671b76dbdfabddc0
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-13 21:51:19 +0900

    test(MenuTest) : 여러 값을 ParameterizedTest로 테스트하도록 수정

--- src/test/java/christmas/domain/menu/MenuTest.java
+++ src/test/java/christmas/domain/menu/MenuTest.java
@@ -3,87 +3,63 @@ package christmas.domain.menu;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MenuTest {
 
     @DisplayName("메뉴판에 존재 여부를 확인하고 있으면 true 없으면 false를 반환한다.")
-    @Test
-    void containsTest() {
-        // given
-        String menuName1 = "양송이수프";
-        String menuName2 = "치킨";
-
+    @ParameterizedTest
+    @CsvSource({"양송이수프, true", "치킨, false"})
+    void containsTest(String menuName, boolean expected) {
         // when
-        boolean containsMenuName1 = Menu.contains(menuName1);
-        boolean containsMenuName2 = Menu.contains(menuName2);
+        boolean containsMenuName = Menu.contains(menuName);
 
         // then
-        assertThat(containsMenuName1).isTrue();
-        assertThat(containsMenuName2).isFalse();
+        assertThat(containsMenuName).isEqualTo(expected);
     }
 
     @DisplayName("메뉴의 가격을 가져온다.")
-    @Test
-    void getPriceOfTest() {
-        // given
-        String menuName1 = "티본스테이크";
-        String menuName2 = "제로콜라";
-
+    @ParameterizedTest
+    @CsvSource({"티본스테이크, 55000", "제로콜라, 3000"})
+    void getPriceOfTest(String menuName, int expectedPrice) {
         // when
-        int priceOfMenuName1 = Menu.getPriceOf(menuName1);
-        int priceOfMenuName2 = Menu.getPriceOf(menuName2);
+        int priceOfMenuName = Menu.getPriceOf(menuName);
 
         // then
-        assertThat(priceOfMenuName1).isEqualTo(55000);
-        assertThat(priceOfMenuName2).isEqualTo(3000);
+        assertThat(priceOfMenuName).isEqualTo(expectedPrice);
     }
 
     @DisplayName("해당 메뉴가 DRINK 카테고리의 메뉴인지 확인한다.")
-    @Test
-    void isDrinkMenuTest() {
-        // given
-        String menuName1 = "레드와인";
-        String menuName2 = "아이스크림";
-
+    @ParameterizedTest
+    @CsvSource({"레드와인, true", "아이스크림, false"})
+    void isDrinkMenuTest(String menuName, boolean expected) {
         // when
-        boolean isDrinkMenu1 = Menu.isDrinkMenu(menuName1);
-        boolean isDrinkMenu2 = Menu.isDrinkMenu(menuName2);
+        boolean isDrinkMenu = Menu.isDrinkMenu(menuName);
 
         // then
-        assertThat(isDrinkMenu1).isTrue();
-        assertThat(isDrinkMenu2).isFalse();
+        assertThat(isDrinkMenu).isEqualTo(expected);
     }
 
     @DisplayName("해당 메뉴가 Dessert 카테고리의 메뉴인지 확인한다.")
-    @Test
-    void isDessertTest() {
-        // given
-        String menuName1 = "레드와인";
-        String menuName2 = "아이스크림";
-
+    @ParameterizedTest
+    @CsvSource({"레드와인, false", "아이스크림, true"})
+    void isDessertMenuTest(String menuName, boolean expected) {
         // when
-        boolean isDessertMenu1 = Menu.isDessert(menuName1);
-        boolean isDessertMenu2 = Menu.isDessert(menuName2);
+        boolean isDessertMenu = Menu.isDessert(menuName);
 
         // then
-        assertThat(isDessertMenu1).isFalse();
-        assertThat(isDessertMenu2).isTrue();
+        assertThat(isDessertMenu).isEqualTo(expected);
     }
 
     @DisplayName("해당 메뉴가 Main 카테고리의 메뉴인지 확인한다.")
-    @Test
-    void isMainTest() {
-        // given
-        String menuName1 = "바비큐립";
-        String menuName2 = "타파스";
-
+    @ParameterizedTest
+    @CsvSource({"바비큐립, true", "타파스, false"})
+    void isMainMenuTest(String menuName, boolean expected) {
         // when
-        boolean isMainMenu1 = Menu.isMain(menuName1);
-        boolean isMainMenu2 = Menu.isMain(menuName2);
+        boolean isMainMenu = Menu.isMain(menuName);
 
         // then
-        assertThat(isMainMenu1).isTrue();
-        assertThat(isMainMenu2).isFalse();
+        assertThat(isMainMenu).isEqualTo(expected);
     }
 }
