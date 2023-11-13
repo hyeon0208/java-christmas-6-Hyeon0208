d22d10732bd1593a85bc643162a3de5f35b1b7b7
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-13 21:22:27 +0900

    refactor(OrderDetail) : OrderMenuName와 Quantity를 필드로 사용하도록 변경

--- src/main/java/christmas/domain/OrderDetail.java
+++ src/main/java/christmas/domain/OrderDetail.java
@@ -1,59 +1,35 @@
 package christmas.domain;
 
-import christmas.constant.ErrorMessage;
 import christmas.util.StringConvertor;
 
 public class OrderDetail {
-    private static final int MIN_ORDER_QUANTITY = 1;
-    private final MenuInfo menuInfo;
-    private final int quantity;
+    private final OrderMenuName name;
+    private final int price;
+    private final Quantity quantity;
 
-    private OrderDetail(MenuInfo menuInfo, int quantity) {
-        this.menuInfo = menuInfo;
+    public OrderDetail(OrderMenuName name, int price, Quantity quantity) {
+        this.name = name;
+        this.price = price;
         this.quantity = quantity;
     }
 
     public static OrderDetail of(String orderMenu) {
         String[] orderInfo = StringConvertor.splitByHyphen(orderMenu);
-        String name = makeValidatedName(orderInfo);
-        int price = Menu.getPriceOf(name);
-        int quantity = makeValidatedQuantity(orderInfo);
-        return new OrderDetail(MenuInfo.of(name, price), quantity);
-    }
-
-    private static String makeValidatedName(String[] orderInfo) {
-        String name = orderInfo[0];
-        validateExistMenu(name);
-        return name;
-    }
-
-    private static void validateExistMenu(String name) {
-        if (!Menu.contains(name)) {
-            throw new IllegalArgumentException(ErrorMessage.ORDER_ERROR);
-        }
-    }
-
-    private static int makeValidatedQuantity(String[] orderInfo) {
-        int quantity = StringConvertor.convertStringToInt(orderInfo[1]);
-        validateOrderQuantity(quantity);
-        return quantity;
-    }
-
-    private static void validateOrderQuantity(int quantity) {
-        if (quantity < MIN_ORDER_QUANTITY) {
-            throw new IllegalArgumentException(ErrorMessage.ORDER_ERROR);
-        }
+        OrderMenuName menuName = OrderMenuName.from(orderInfo);
+        int price = Menu.getPriceOf(menuName);
+        Quantity quantity = Quantity.from(orderInfo);
+        return new OrderDetail(menuName, price, quantity);
     }
 
     public String getMenuName() {
-        return menuInfo.getName();
+        return name.getName();
     }
 
     public int getQuantity() {
-        return quantity;
+        return quantity.getQuantity();
     }
 
     public int getOrderPrice() {
-        return menuInfo.getPrice() * quantity;
+        return price * quantity.getQuantity();
     }
 }
