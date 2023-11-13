23607267f8deb162f1c131a314de9beb87691336
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-13 21:20:35 +0900

    feat(Quantity) : 주문 개수를 포장하는 객체 추가

---
+++ src/main/java/christmas/domain/Quantity.java
@@ -0,0 +1,29 @@
+package christmas.domain;
+
+import christmas.constant.ErrorMessage;
+import christmas.util.StringConvertor;
+
+public class Quantity {
+    private static final int MIN_ORDER_QUANTITY = 1;
+    private int quantity;
+
+    private Quantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public static Quantity from(String[] orderInfo) {
+        int quantity = StringConvertor.convertStringToInt(orderInfo[1]);
+        validateOrderQuantity(quantity);
+        return new Quantity(quantity);
+    }
+
+    private static void validateOrderQuantity(int quantity) {
+        if (quantity < MIN_ORDER_QUANTITY) {
+            throw new IllegalArgumentException(ErrorMessage.ORDER_ERROR);
+        }
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
