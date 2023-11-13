1761e7f99f5943b9b7eaddfd1267a390545f3ba1
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-13 17:21:31 +0900

    feat(ErrorView) : 에러 출력을 담당하는 클래스 추가

--- src/main/java/christmas/constant/ErrorMessage.java
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -1,8 +1,8 @@
 package christmas.constant;
 
 public class ErrorMessage {
-    public static final String VISIT_DATE_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    public static final String ORDER_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String VISIT_DATE_ERROR = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String ORDER_ERROR = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     private ErrorMessage() {
     }
