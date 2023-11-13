35f4c9c4928b920b6f07bedfb7c0ac76080dc7f6
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-13 16:46:44 +0900

    refactor(OutputView) : println으로 출력 후 줄바꿈을 추가하는 기능 추가하여 반복 로직 제거

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -14,64 +14,65 @@ public class OutputView {
     }
 
     public void printBenefitPreviewMessageFor(User user) {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", user.getVisitDate());
-        System.out.println();
+        String text = String.format("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", user.getVisitDate());
+        printlnWithNewLine(text);
     }
 
     public void printMenu(User user) {
         System.out.println("<주문 메뉴>");
-        StringBuilder orderMenus = new StringBuilder();
-        user.getOrderDetails().stream()
+        String text = user.getOrderDetails().stream()
                 .map(orderDetail -> String.format("%s %s개", orderDetail.getMenuName(), orderDetail.getQuantity()))
-                .forEach(menu -> orderMenus.append(menu).append("\n"));
-        System.out.println(orderMenus);
+                .collect(Collectors.joining("\n"));
+        printlnWithNewLine(text);
     }
 
     public void printTotalOrderPriceFor(User user) {
         System.out.println("<할인 전 총주문 금액>");
-        System.out.printf("%,d원\n", user.getTotalOrderPrice());
-        System.out.println();
+        String text = String.format("%,d원", user.getTotalOrderPrice());
+        printlnWithNewLine(text);
     }
 
     public void printGiftMenu(User user) {
         System.out.println("<증정 메뉴>");
-        String output = Gift.findByTotalOrderPrice(user.getTotalOrderPrice())
+        String text = Gift.findByTotalOrderPrice(user.getTotalOrderPrice())
                 .map(gift -> String.format("%s %d개", gift.getProduct(), gift.getQuantity()))
                 .orElse("없음");
-        System.out.println(output);
-        System.out.println();
+        printlnWithNewLine(text);
     }
 
     public void printBenefitDetails(Benefit benefit) {
         System.out.println("<혜택 내역>");
-        String output = "없음";
+        String text = "없음";
         if (benefit.isAnyAppliedBenefit()) {
-            output = benefit.getAppliedBenefit().stream()
+            text = benefit.getAppliedBenefit().stream()
                     .map(eventInfo -> String.format("%s: -%,d원", eventInfo.name(), eventInfo.discountPrice()))
                     .collect(Collectors.joining("\n"));
         }
-        System.out.println(output);
-        System.out.println();
+        printlnWithNewLine(text);
     }
 
     public void printTotalBenefitPrice(Benefit benefit) {
         System.out.println("<총혜택 금액>");
-        String output = "0원";
+        String text = "0원";
         if (benefit.isAnyAppliedBenefit()) {
-            output = String.format("-%,d원",  benefit.getTotalBenefitPrice());
+            text = String.format("-%,d원",  benefit.getTotalBenefitPrice());
         }
-        System.out.println(output);
-        System.out.println();
+        printlnWithNewLine(text);
     }
 
     public void printActualPaymentPrice(Payment payment) {
         System.out.println("<할인 후 예상 결제 금액>");
-        System.out.printf("%,d원\n", payment.getActualPaymentPrice());
-        System.out.println();
+        String text = String.format("%,d원", payment.getActualPaymentPrice());
+        printlnWithNewLine(text);
     }
 
     public void printEventBadge(Benefit benefit) {
         System.out.println("<12월 이벤트 배지>");
         System.out.println(Badge.giveEventBadge(benefit.getTotalBenefitPrice()));
+    }
+
+    private void printlnWithNewLine(String text) {
+        System.out.println(text);
+        System.out.println();
     }
 }
