c18fccebf7cdc79e26bbc04196ff3f0209cf0d44
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-13 16:28:35 +0900

    feat(OutputView) : 적용된 혜택 내역 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,7 +1,11 @@
 package christmas.view;
 
+import christmas.domain.Badge;
+import christmas.domain.Benefit;
 import christmas.domain.Gift;
+import christmas.domain.Payment;
 import christmas.domain.User;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
@@ -34,6 +38,18 @@ public class OutputView {
         String output = Gift.findByTotalOrderPrice(user.getTotalOrderPrice())
                 .map(gift -> String.format("%s %d개", gift.getProduct(), gift.getQuantity()))
                 .orElse("없음");
+        System.out.println(output);
+        System.out.println();
+    }
+
+    public void printBenefitDetails(Benefit benefit) {
+        System.out.println("<혜택 내역>");
+        String output = "없음";
+        if (benefit.isAnyAppliedBenefit()) {
+            output = benefit.getAppliedBenefit().stream()
+                    .map(eventInfo -> String.format("%s: -%,d원", eventInfo.name(), eventInfo.discountPrice()))
+                    .collect(Collectors.joining("\n"));
+        }
         System.out.println(output);
         System.out.println();
     }
