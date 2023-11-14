698669d92b16aae6143f78cd3d47fa54800cd64a
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-14 12:06:19 +0900

    test(OrderDetailTest) : 주문 상세에 대한 테스트를 하도록 수정

--- src/test/java/christmas/domain/user/order/OrderDetailTest.java
+++ src/test/java/christmas/domain/user/order/OrderDetailTest.java
@@ -3,22 +3,26 @@ package christmas.domain.user.order;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class OrderDetailTest {
 
-    @DisplayName("주문한 메뉴 가격과 개수를 곱한 주문 금액을 구한다.")
-    @Test
-    void getOrderPriceTest() {
+    @DisplayName("주문 상세 정보를 가져온다.")
+    @ParameterizedTest
+    @CsvSource({"바비큐립-2, 바비큐립, 2, 108000", "해산물파스타-5, 해산물파스타, 5, 175000"})
+    void getOrderPriceTest(String orderInfo, String expectedMenuName, int expectedQuantity, int expectedPrice) {
         // given
-        String orderMenu = "크리스마스파스타-2";
+        OrderDetail orderDetail = OrderDetail.from(orderInfo);
 
         // when
-        OrderDetail orderDetail = OrderDetail.of(orderMenu);
+        String menuName = orderDetail.getMenuName();
+        int quantity = orderDetail.getQuantity();
         int orderPrice = orderDetail.getOrderPrice();
-        int expectedOrderPrice = 50000;
 
         // then
-        assertThat(orderPrice).isEqualTo(expectedOrderPrice);
+        assertThat(menuName).isEqualTo(expectedMenuName);
+        assertThat(quantity).isEqualTo(expectedQuantity);
+        assertThat(orderPrice).isEqualTo(expectedPrice);
     }
 }
